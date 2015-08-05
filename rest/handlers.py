99117c16c9637ffa71326270706ef85bfb45ad1c
Rodrigo Savian <rodrigosavian@gmail.com>
2015-08-05 00:52:51 -0300

    Test api list view with limit param.

--- rest/handlers.py
+++ rest/handlers.py
@@ -9,12 +9,19 @@ class BaseRequestHandler(RequestHandler):
 
     model = None
     serializer_class = None
+    limit = None
 
     def get_queryset(self):
         return self.model.query().filter()
 
     def get_object_list(self):
-        return self.get_queryset().all()
+        queryset = self.get_queryset()
+
+        limit = self.get_limit()
+        if limit:
+            queryset = queryset.limit(limit)
+
+        return queryset.all()
 
     def get_object(self):
         queryset = self.get_queryset()
@@ -22,6 +29,10 @@ class BaseRequestHandler(RequestHandler):
 
     def get_serializer(self, *args, **kwargs):
         return self.serializer_class(*args, **kwargs)
+
+    def get_limit(self):
+        limit = self.get_argument('limit', '')
+        return limit or self.limit or None
 
 
 class ListRequestHandler(
---
+++ test.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import json
+
+from tornado.testing import AsyncHTTPTestCase
+
+import app
+from settings import db
+
+from apps.people.models import Person
+
+
+class PersonListAPITest(AsyncHTTPTestCase):
+    model_class = Person
+
+    def get_app(self):
+        # first clear all
+        db.query(self.model_class).delete()
+
+        for x in range(5):
+            p = self.model_class(
+                    username=u'rodrigocesar.savian%s' % x,
+                    facebook_id='100003194166055%s' % x,
+                    name=u'Rodrigo Cesar Savian%s' % x,
+                    gender=u'male')
+            db.add(p)
+        db.commit()
+        self.object_list = db.query(self.model_class).all()
+        self.object = self.object_list[0]
+        return app.make_app_test()
+
+    def test_status_code_person_list(self):
+        response = self.fetch('/person/')
+        self.assertEqual(response.code, 200)
+
+    def test_without_limit_person_list(self):
+        response = self.fetch('/person/')
+        body = '[{"username": "rodrigocesar.savian0", "facebookId": "1000031941660550", "name": "Rodrigo Cesar Savian0", "gender": "male"}, {"username": "rodrigocesar.savian1", "facebookId": "1000031941660551", "name": "Rodrigo Cesar Savian1", "gender": "male"}, {"username": "rodrigocesar.savian2", "facebookId": "1000031941660552", "name": "Rodrigo Cesar Savian2", "gender": "male"}, {"username": "rodrigocesar.savian3", "facebookId": "1000031941660553", "name": "Rodrigo Cesar Savian3", "gender": "male"}, {"username": "rodrigocesar.savian4", "facebookId": "1000031941660554", "name": "Rodrigo Cesar Savian4", "gender": "male"}]'
+
+        dict_object = json.loads(response.body)
+        self.assertEqual(response.body, body)
+        self.assertEqual(len(dict_object), 5)
+
+    def test_limit_two_person_list(self):
+        response = self.fetch('/person/?limit=2')
+        body = '[{"username": "rodrigocesar.savian0", "facebookId": "1000031941660550", "name": "Rodrigo Cesar Savian0", "gender": "male"}, {"username": "rodrigocesar.savian1", "facebookId": "1000031941660551", "name": "Rodrigo Cesar Savian1", "gender": "male"}]'
+
+        dict_object = json.loads(response.body)
+        self.assertEqual(response.code, 200)
+        self.assertEqual(response.body, body)
+        self.assertEqual(len(dict_object), 2)
