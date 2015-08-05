b4189d1c6fd76c2ce7983ee175821296fd025aba
Rodrigo Savian <rodrigosavian@gmail.com>
2015-08-05 00:51:16 -0300

    Refactoring urls.

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ from apps.people.apis.v1.api import PersonRetrieveAPI
 
 
 application = tornado.web.Application([
-    (r'/person$',  PersonListAPI),
-    (r'/person/(\d+)$',  PersonRetrieveAPI),
+    (r'/person/$',  PersonListAPI),
+    (r'/person/(\d+)/$',  PersonRetrieveAPI),
 
 ])
+
+def make_app_test():
+    global application
+    return application
 
 if __name__ == '__main__':
     application.listen(8888)
